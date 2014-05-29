5417eb67ecb2a266e4aa35e6c18b8c758f53bdc3
Per Lundholm <per@artcomputer.se>
2014-05-29 10:54:30 +0200

    Robustare struktur för att hantera partiella EXIF. Loggar allt i en fil. Faller tillbaka på last modified när data saknas. Quicktime "mov" får modifieringsdatum som namn.

--- src/main/java/RenameToDates.java
+++ src/main/java/RenameToDates.java
@@ -4,12 +4,24 @@ import com.drew.metadata.Metadata;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class RenameToDates {
+    private static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH.mm.ss");
+    private static String currentLog = "";
+    private static PrintWriter logStream;
+
     public static void main(String[] args) throws ImageProcessingException, IOException {
+        File dir = checkArgumentIsAFolderOrExit(args);
+        openLogStream();
+        loopOverDirectoryAndRenameFiles(dir);
+    }
+
+    private static File checkArgumentIsAFolderOrExit(String[] args) {
         if (args.length != 1) {
             System.err.println("Usage: folder");
             System.exit(1);
@@ -19,42 +31,109 @@ public class RenameToDates {
             System.err.println(args[0] + " is not a directory");
             System.exit(2);
         }
-        final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH.mm.ss");
-        for (File jpegFile : dir.listFiles()) {
-            if (jpegFile.getName().endsWith("jpg")) {
-                Metadata metadata = ImageMetadataReader.readMetadata(jpegFile);
-                final ExifSubIFDDirectory exifSubIFDDirectory = metadata.getDirectory(ExifSubIFDDirectory.class);
-                final Date date = exifSubIFDDirectory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+        return dir;
+    }
 
-                System.out.print(jpegFile.getName() + ": ");
-                if (date == null) {
-                    System.out.println("No date");
+    private static void openLogStream() {
+        try {
+            logStream = new PrintWriter(format.format(new Date()) + ".log");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(3);
+        }
+    }
+
+    private static void loopOverDirectoryAndRenameFiles(File dir) throws ImageProcessingException, IOException {
+        logNl("Doing folder '" + dir.getAbsolutePath() + "'");
+        for (File file : dir.listFiles()) {
+            if (file.isDirectory()) {
+                loopOverDirectoryAndRenameFiles(file);
+            } else {
+                if (fileEndsWith(file, "jpg")) {
+                    renameJpgFile(file);
+                } else if (fileEndsWith(file, "mov")) {
+                    renameMovFile(file);
                 } else {
-                    final String newName = format.format(date);
-                    System.out.println(newName);
-                    tryRename(jpegFile, newName);
+                    logNl("Skip file: '" + file.getName() + "'");
                 }
             }
         }
+    }
 
+
+
+    private static boolean fileEndsWith(File file, String suffix) {
+        return file.getName().toLowerCase().endsWith(suffix);
+    }
+
+    private static void renameMovFile(File file) {
+        log(file.getName() + ": ");
+        final Date date = new Date(file.lastModified());
+        final File destinationFileName = createDestinationFileName(file, format.format(date), ".mov");
+        doRename(file, destinationFileName);
+    }
+
+    private static void log(String message) {
+        System.out.print(message);
+        currentLog += message;
+    }
+
+    private static void logNl(String message) {
+        log(message);
+        System.out.println();
+        logStream.println(currentLog);
+        currentLog = "";
+    }
+    private static void renameJpgFile(File jpegFile) throws ImageProcessingException, IOException {
+        log(jpegFile.getName() + ": ");
+        Date date = null;
+        Metadata metadata = ImageMetadataReader.readMetadata(jpegFile);
+        if (metadata == null) {
+            logNl("No metadata.");
+        } else {
+            final ExifSubIFDDirectory exifSubIFDDirectory = metadata.getDirectory(ExifSubIFDDirectory.class);
+            if (exifSubIFDDirectory == null) {
+                logNl("No Exif IF directory.");
+            } else {
+                date = exifSubIFDDirectory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+            }
+        }
+        if (date == null) {
+            date = new Date(jpegFile.lastModified());
+            log("No date in Exif, using last modified instead: " + format.format(date) + " ");
+        }
+        final String newName = format.format(date);
+        logNl(newName);
+        tryRename(jpegFile, newName);
     }
 
     private static void tryRename(File jpegFile, String newName) {
         while (true) {
-            final File destination = new File(jpegFile.getParent() + File.separator + newName + ".jpg");
+            final File destination = createDestinationFileName(jpegFile, newName, ".jpg");
             if (destination.exists()) {
-                System.out.println(destination.getAbsolutePath() + " already exists.");
-                if (destination.getAbsolutePath().equals(jpegFile.getAbsolutePath())) {
-                    System.out.println("Already renamed.");
+                if (destination.getAbsolutePath().equalsIgnoreCase(jpegFile.getAbsolutePath())) {
+                    logNl("Already renamed: " + destination.getAbsolutePath());
                     return;
-                }
-                else {
+                } else {
                     newName += "x";
+                    tryRename(jpegFile, newName);
                 }
             } else {
-                jpegFile.renameTo(destination);
+                doRename(jpegFile, destination);
                 return;
             }
+        }
+    }
+
+    private static File createDestinationFileName(File jpegFile, String newName, String suffix) {
+        return new File(jpegFile.getParent() + File.separator + newName + suffix);
+    }
+
+    private static void doRename(File source, File destination) {
+        logNl(String.format("Rename from '%s' to '%s'", source, destination));
+        if (!source.renameTo(destination)) {
+            logStream.flush();
+            throw new RuntimeException("Rename failed for " + source.getAbsolutePath());
         }
     }
 }
